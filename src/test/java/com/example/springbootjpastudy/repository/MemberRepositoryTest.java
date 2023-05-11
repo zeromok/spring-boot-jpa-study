ba0e83b7f0626c1ba9d91b660a0d7b0c3b1c8708
Daniel <byoung.mokk@gmail.com>
2023-05-11 17:00:36 +0900

    Repository Interface 메서드 실습(2)

--- src/test/java/com/example/springbootjpastudy/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/springbootjpastudy/repository/MemberRepositoryTest.java
@@ -4,9 +4,11 @@ import com.example.springbootjpastudy.domain.Member;
 import jakarta.transaction.Transactional;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.EndsWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -36,9 +38,55 @@ class MemberRepositoryTest {
 //        System.out.println(member);
 
         // 없으면 null 반환해
-        Member member = userRepository.findById(10L).orElse(null);
-        System.out.println(member);
+//        Member member = userRepository.findById(10L).orElse(null);
+//        System.out.println(member);
 
+        // Flush : DB 반영시점을 조절해 준다.
+//        Member member = userRepository.saveAndFlush(new Member("saveAndFlush", "test"));
+//        userRepository.findAll().forEach(System.out::println);
+
+//        long count = userRepository.count();
+//        System.out.println("===> " + count);
+
+        // count 를 사용해 확인한다.
+//        boolean exist = userRepository.existsById(2L);
+//        System.out.println("===> " + exist);
+
+        // delete 할 때 먼저 select 로 확인 후 실행 -> 성능이슈 이유
+//        userRepository.deleteById(2L);
+//        userRepository.findAll().forEach(System.out::println);
+
+//        userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(2L, 4L)));
+//        userRepository.findAll().forEach(System.out::println);
+
+        // 성능이슈 해결법 1.
+//        userRepository.deleteAllInBatch(userRepository.findAllById(Lists.newArrayList(2L, 4L)));
+//        userRepository.findAll().forEach(System.out::println);
+
+        // 성능이슈 해결법 2.
+//        userRepository.deleteInBatch(userRepository.findAllById(Lists.newArrayList(2L, 4L)));
+//        userRepository.findAll().forEach(System.out::println);
+
+        // 페이징 관련 메서드
+//        Page<Member> members = userRepository.findAll(PageRequest.of(1, 3));
+//        System.out.println("===> " + members);
+//        System.out.println("page : " + members);
+//        System.out.println("totalElements : " + members.getTotalElements());
+//        System.out.println("totalPages : " + members.getTotalPages());
+//        System.out.println("numberOfElements : " + members.getNumberOfElements());
+//        System.out.println("sort : " + members.getSort());
+//        System.out.println("size : " + members.getSize());
+//        members.getContent().forEach(System.out::println);
+
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("name")
+                .withIgnorePaths("id")
+                .withIgnorePaths("createdAt")
+                .withIgnorePaths("updatedAt")
+                .withMatcher("email", endsWith());
+
+        Example<Member> example = Example.of(new Member("ma", "fast@gmail.com"), matcher);
+        userRepository.findAll(example).forEach(System.out::println);
 
     }
 
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,11 +1,11 @@
 
 insert into MEMBER (`id`, `name`, `email`, `created_at`, `updated_at`)
-values ( 1, 'Daniel', 'Daniel@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
+values ( 2, 'Daniel', 'Daniel@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
 insert into MEMBER (`id`, `name`, `email`, `created_at`, `updated_at`)
-values ( 2, 'Zeromok', 'Zeromok@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
+values ( 3, 'Zeromok', 'Zeromok@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
 insert into MEMBER (`id`, `name`, `email`, `created_at`, `updated_at`)
-values ( 3, 'Test', 'test@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
+values ( 4, 'Test', 'test@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
 insert into MEMBER (`id`, `name`, `email`, `created_at`, `updated_at`)
-values ( 4, 'Test', 'test2@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
+values ( 5, 'Test', 'test2@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
 insert into MEMBER (`id`, `name`, `email`, `created_at`, `updated_at`)
-values ( 5, 'fast', 'fast@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
+values ( 6, 'fast', 'fast@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
