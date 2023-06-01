7f8b0282037a182e51f74a29bef0b0c0536bd10e
Daniel <byoung.mokk@gmail.com>
2023-06-01 17:33:00 +0900

    Study: 9단계 - logging 찍먹
    1. banner 변경

--- src/main/java/com/example/springbootjpastudy/login/LoginController.java
+++ src/main/java/com/example/springbootjpastudy/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.springbootjpastudy.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @GetMapping("/login")
     public String gotoLoginPage(@RequestParam String name, ModelMap modelMap) {
-        System.out.println("RequestParam is " + name);
+//        System.out.println("RequestParam is " + name);
+
+        logger.trace("trace");
+        logger.debug("RequestParam is {}", name);
+        logger.info("info");
+        logger.error("error");
+        logger.warn("warn");
+
+
 
         modelMap.put("name", name);
 
